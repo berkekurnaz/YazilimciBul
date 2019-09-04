46bc13cf75ae9feef0d865979adb4aae13f6cd4c
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-09-04 23:45:16 +0300

    Add Backend Website Template

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET docs page. */
+router.get('/docs', function(req, res, next) {
+  res.render('docshome');
+});
+
+/* GET docs page. */
+router.get('/docs/usage', function(req, res, next) {
+  res.render('docsusage');
+});
+
+/* GET docs page. */
+router.get('/docs/developer', function(req, res, next) {
+  res.render('docsdeveloper');
+});
+
 module.exports = router;
